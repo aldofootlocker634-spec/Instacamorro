56b23430d61d95c2153801f6deef2307a7def23a
aldofootlocker634-spec <aldofootlocker634@gmail.com>
2026-05-30 23:44:32 +0200

    Create constants.dart

---
+++ constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static const String appName = "Adhan Italia";
+  static const String takbirFile = "assets/audio/takbir.mp3";
+  
+  // Kaaba coordinates for Qibla
+  static const double kaabaLat = 21.4225;
+  static const double kaabaLon = 39.8261;
+  
+  // Default Calculation Method (Muslim World League)
+  static const int calcMethod = 2; 
+}
